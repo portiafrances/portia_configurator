830b516dfddfb8b4594d7b19463a16589def1b44
haederth <thomas.haederle@ubs.com>
2017-12-19 15:44:16 +0000

    Renamed project

--- README.rst
+++ README.rst
@@ -2,21 +2,21 @@
 configurator
 ===============================
 
-.. image:: https://img.shields.io/pypi/v/configurator.svg
-        :target: https://pypi.python.org/pypi/configurator
+.. image:: https://img.shields.io/pypi/v/portia_configurator.svg
+        :target: https://pypi.python.org/pypi/portia_configurator
 
-.. image:: https://img.shields.io/travis/tomanizer/configurator.svg
-        :target: https://travis-ci.org/tomanizer/configurator
+.. image:: https://img.shields.io/travis/tomanizer/portia_configurator.svg
+        :target: https://travis-ci.org/tomanizer/portia_configurator
 
-.. image:: https://readthedocs.org/projects/configurator/badge/?version=latest
-        :target: https://readthedocs.org/projects/configurator/?badge=latest
+.. image:: https://readthedocs.org/projects/portia_configurator/badge/?version=latest
+        :target: https://readthedocs.org/projects/portia_configurator/?badge=latest
         :alt: Documentation Status
 
 
 Configurator creates an appconfig from the commandline or a config file for any batch-style application.
 
 * Free software: MIT license
-* Documentation: https://configurator.readthedocs.org.
+* Documentation: https://portia_configurator.readthedocs.org.
 
 Features
 --------
@@ -25,7 +25,10 @@ Features
 * Configure templates to run on different dates
 * Configure emails to sendto on failure
 * Automatically log all runs
- 
+* Two Styles
+    - configurator: use config files and command line to configure appconfig
+    - ini configurator: use dos-style ini files to configure
+
 
 Credits
 ---------
--- setup.py
+++ setup.py
@@ -1,15 +1,67 @@
-import os
-del os.link
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from setuptools import setup
+import pip
 
-setup(name='configurator',
-      version='0.8.4',
-      description='Include in projects which need configuration from commandline and config files',
-      url='https://github.com/tomanizer/configurator',
-      author='Thomas Haederle',
-      author_email='thomas.haederle@gmail.com',
-      license='MIT',
-      packages=['configurator'],
-      install_requires=['argparseui', 'ConfigParser', 'argparse', 'PyQt4',],
-      zip_safe=False)
+from pip.req import parse_requirements
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+parsed_requirements = parse_requirements(
+    'requirements/prod.txt',
+    session=pip.download.PipSession()
+)
+
+parsed_test_requirements = parse_requirements(
+    'requirements/test.txt',
+    session=pip.download.PipSession()
+)
+
+
+requirements = [str(ir.req) for ir in parsed_requirements]
+test_requirements = [str(tr.req) for tr in parsed_test_requirements]
+
+
+setup(
+    name='configurator',
+    version='0.1.0',
+    description="Configurator creates an appconfig from the commandline or a config file for any batch-style application.",
+    long_description=readme + '\n\n' + history,
+    author="Thomas Haederle",
+    author_email='thomas.haederle@portiafrances.com',
+    url='https://github.com/tomanizer/portia_configurator',
+    packages=[
+        'configurator',
+    ],
+    package_dir={'configurator':
+                 'configurator'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='configurator',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities'
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
